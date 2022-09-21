bdbbc963927e79a53529555dfff87880b775f085
nileshbhimani <bhimaninilesh16@gmail.com>
2022-09-21 21:00:14 +0530

    Update array_slice.go

--- array_slice.go
+++ array_slice.go
@@ -34,8 +34,56 @@ func main() {
 
            fmt.Println("new key word append",newKeyWord)
 
+
+        a := make([]int, 2, 5)
+        a[0] = 10
+        a[1] = 20
+
+        a = append(a,30,40,50)
+        fmt.Println("value of a",a)
+
+        var b = make([]int,8,16)
+
+        copy(b,a)
+
+        fmt.Println("Copy of a to b == b",b)
+
+
+        var strArr = []string {"ni","nil"}
+
+        strArr = RemoveIndex(strArr,0)
+         fmt.Println("Removed value of a",strArr)
+
         fmt.Println(reflect.ValueOf(array).Kind())
         fmt.Println(reflect.ValueOf(stringarray).Kind())
 
+
+       var strSlice = []string{"India", "Canada", "Japan", "Germany", "Italy"}
+        fmt.Println(itemExists(strSlice, "Canada"))
+        fmt.Println(itemExists(strSlice, "Africa")) 
+
 }
 
+func itemExists( Arr interface{}, t interface{}) bool{
+    s := reflect.ValueOf(Arr)
+
+    fmt.Println("Value of if Exist check",s)
+
+    // for index,value := range Arr{
+    //     if (value == t){
+    //         return true    
+    //     }
+    // }    
+
+    for i:=0;i<s.Len();i++{
+        if ( s.Index(i).Interface()==t){
+            return true
+        } 
+    }
+
+    return false
+}
+
+func RemoveIndex(s []string, index int) []string {
+	return append(s[:index], s[index+1:]...)
+}
