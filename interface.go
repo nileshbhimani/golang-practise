a328fd94da24aba01d5d265f7d0da29a29bcdd82
nileshbhimani <bhimaninilesh16@gmail.com>
2022-09-22 16:59:19 +0530

    Update interface.go

--- interface.go
+++ interface.go
@@ -154,3 +154,72 @@ func main() {
 		fmt.Println(polygons[i].Edges())
 	}
 }
+
+================== Polymorphisam using struct =========================================
+// Golang program to illustrate the
+// concept of interfaces
+package main
+ 
+import (
+    "fmt"
+)
+ 
+// defining an interface
+type Figure interface{
+ 
+    Area() float64
+}
+ 
+// declaring a struct
+type Rectangle struct{
+     
+    // declaring struct variables
+    length float64
+    width float64
+}
+ 
+// declaring a struct
+type Square struct{
+     
+    // declaring struct variable
+    side float64
+}
+ 
+// function to calculate
+// area of a rectangle
+func (rect Rectangle) Area() float64{
+ 
+    // Area of rectangle = l * b
+    area := rect.length * rect.width
+    return area
+}
+ 
+// function to calculate
+// area of a square
+func (sq Square) Area() float64{
+     
+    // Area of square = a * a
+    area := sq.side * sq.side
+    return area
+}
+ 
+// main function
+func main() {
+     
+    // declaring a rectangle instance
+    rectangle := Rectangle{
+     
+        length: 10.5,
+        width: 12.25,
+    }
+     
+    // declaring a square instance
+    square := Square{
+     
+        side: 15.0,
+    }
+     
+    // printing the calculated result
+    fmt.Printf("Area of rectangle: %.3f unit sq.\n", rectangle.Area())
+    fmt.Printf("Area of square: %.3f unit sq.\n", square.Area())
+}
